a61cc5925e4d0660244cd884b2342e1fbc66e456
Egil Moeller <egil.moller@freecode.no>
2011-02-07 13:41:11 +0100

    Bugfix

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
+#! /bin/bash
 cd apps
-git clone git@github.com:freecode/fcdjangoutils.git
+git clone git://github.com/freecode/fcdjangoutils.git
 git clone git://github.com/freecode/djangoobjfeed.git
 git clone git://github.com/freecode/routeedit.git
